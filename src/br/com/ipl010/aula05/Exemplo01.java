7297980e96bcb6c643fed4484c293d63df9fe697
Douglas <Douglas@POS-NOT-WIN8>
2019-04-11 16:54:08 -0700

    Commit de exemplo 1 aula 5 de ILP010.

---
+++ src/br/com/ipl010/aula05/Exemplo01.java
@@ -0,0 +1,12 @@
+package br.com.ipl010.aula05;
+
+public class Exemplo01 {
+	public static void main(String[] args) {
+		
+		int a = 65;
+		int b = 32;
+		
+		System.out.println("A = " + a +"\nB = " + b + "\nA + B = " + (a+b));
+	}
+
+}
