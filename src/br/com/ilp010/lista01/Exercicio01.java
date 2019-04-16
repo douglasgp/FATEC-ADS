659f751b7736d575e3152591e9df2eb46d5b0882
dougl_000 <dougl_000@192.168.5.249>
2019-04-16 10:52:25 -0700

    Resolução de conflito com git.

--- src/br/com/ilp010/lista01/Exercicio01.java
+++ src/br/com/ilp010/lista01/Exercicio01.java
@@ -6,6 +6,7 @@ public class Exercicio01 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String nome = "Ferdinando";
+
 		int repete = 10;
 		// Estrutura para repetir o nome
 		for (int i = 0; i < 10; i++) {
