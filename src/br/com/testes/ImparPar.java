940417610bd64fb77048f495c2def3da69990753
dougl_000 <dougl_000@192.168.5.246>
2019-04-21 21:42:17 -0700

    Conditional presentation of odd or even values ​​using vector in Java.

---
+++ src/br/com/testes/ImparPar.java
@@ -0,0 +1,20 @@
+package br.com.testes;
+
+import java.util.Scanner;
+
+public class ImparPar {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// Variáveis
+		int num = 10;
+		int valor[] = { 12, 23, 36, 48, 51, 62, 73, 84, 95, 101 };
+		// Entrada de dados
+		for (int i = 0; i <= num; i++) {
+			if(valor[i]%2 == 0) {
+				System.out.println(valor[i] + " é PAR");
+			}else {
+				System.out.println(valor[i] + " é ÍMPAR");
+			}
+		}
+	}
+}
