45ab5693791219fc9fed7b48bbd4765e6e5d9d22
dougl_000 <dougl_000@192.168.5.140>
2019-04-24 03:22:16 -0700

    Teste commit.

---
+++ src/br/com/ilp010/lista02/Ex05.java
@@ -0,0 +1,20 @@
+package br.com.ilp010.lista02;
+
+import java.util.Scanner;
+
+/*
+ * Escreva um programa que utilize uma função para determinar o
+ * número do dia do ano, recebendo para isso os parâmetros inteiros
+ * dia, mês e ano. P.ex:15/01/2019 -> 15, 01/04/2019 -> 91
+ */
+public class Ex05 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// Variáveis
+		int data;
+		// Entrada de dados
+		System.out.println("Informe uma data: ");
+		
+		// Invocação das funções
+	}
+}
