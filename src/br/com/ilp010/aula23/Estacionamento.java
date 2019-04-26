00435ca72631bc3933f1390d44d0f1961f69bf31
dougl_000 <dougl_000@192.168.5.140>
2019-04-26 06:33:13 -0700

    Comentários para desenvolvimento de Sistema completo para uso de estacionamento.

---
+++ src/br/com/ilp010/aula23/Estacionamento.java
@@ -0,0 +1,28 @@
+package br.com.ilp010.aula23;
+
+public class Estacionamento {
+
+	// programa principal se ocupa apenas do menu principal,
+	// i.e., de sua exibição e do acionamento das opções
+
+	// função que exibe um mapa das vagas,
+	// mostrando os carros que estão estacionados em cada uma
+
+	// função que localiza um veículo estacionado,
+	// indicado a vaga que ocupa e o horário de entrada
+
+	// função que efetua a saída um veículo estacionado,
+	// localizando a vaga que ocupa e calculando o tempo estacionado
+
+	// função que efetua a entrada de um veículo,
+	// localizando uma vaga livre e registrando o horário de entrada
+
+	// função que localiza a vaga ocupada por um veículo
+
+	// função que localiza uma vaga livre
+
+	// função que lê uma placa de veículo
+
+	// função que exibe o menu principal
+
+}
