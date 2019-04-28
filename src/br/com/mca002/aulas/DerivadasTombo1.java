caa4a32ce22c35a14a138a86ce8fb68d1e6ea2e7
dougl_000 <dougl_000@POS-NOT-WIN8>
2019-04-28 12:46:26 -0700

    Example of Tombo Rule for calculating n derived from a function.

---
+++ src/br/com/mca002/aulas/DerivadasTombo1.java
@@ -0,0 +1,32 @@
+package br.com.mca002.aulas;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class DerivadasTombo1 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// Variáveis
+		int derivada = 0;
+		int coef;
+		int pot = 0;
+		int funcao;
+		// Entrada de dados
+		System.out.println("Informe o valor do coeficiente: ");
+		coef = scan.nextInt();
+		System.out.println("Informe o valor da potência: ");
+		pot = scan.nextInt();
+		/*
+		 * coef *= pot; pot -= 1; System.out.println("Potência = " + pot +
+		 * "\nCoeficiente = " + coef);
+		 */
+		for (int i = pot; i >= 0; i--) {
+			derivada += 1;
+			coef *= pot;
+			pot -= 1;
+			System.out.println("\nDerivada " + derivada + "º grau" + "\nCoeficiente = " + coef + "\nPotência = " + pot);
+			System.out.println(coef + "x ^" + pot);
+		}
+	}
+}
