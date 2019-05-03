949b2bfc84a8b8312a1869c8506583ba2663e1cc
dougl_000 <dougl_000@192.168.5.246>
2019-05-03 00:51:44 -0700

    Funcionalidades básicas para escrita em arquivo txt usando java.io.

---
+++ src/br/com/ilp010/aula24/EscreveTexto.java
@@ -0,0 +1,18 @@
+package br.com.ilp010.aula24;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class EscreveTexto {
+	public static void main(String[] args) throws Exception{
+		PrintWriter pw = new PrintWriter(new FileWriter("arquivo2.txt"));
+		// PrintWriter pw = new PrintWriter(new FileWriter("arquivo2.txt"));
+		pw.println("Início do arquivo");
+		for(int i = 0; i < 5; i++) {
+			pw.print(i);
+			pw.print(", ");
+		}
+		pw.println("Fim");
+		pw.close();
+	}
+}
