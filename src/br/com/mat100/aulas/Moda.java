54e83c41cf97ed3f0baf66ff8f8b9ce5ebebf2f5
dougl_000 <dougl_000@POS-NOT-WIN8>
2019-05-02 23:18:31 -0700

    Declaração de variáveis.

---
+++ src/br/com/mat100/aulas/Moda.java
@@ -0,0 +1,17 @@
+package br.com.mat100.aulas;
+
+import java.util.Scanner;
+
+public class Moda {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// Declaração das variáveis
+		int num = 20;
+		int vetor[] = {2,2,3,3,3,5,5,7,7,7,8,8,8,8,8,8,8,9,9};
+		int aux = 0;
+		
+		for(int i = 0; i < num; i++) {
+			
+		}
+	}
+}
