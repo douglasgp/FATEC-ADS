6eac21d44afd3cbd8a7d301443d79cb0b1c32fb5
dougl_000 <dougl_000@192.168.5.246>
2019-05-03 00:54:09 -0700

    Funcionalidade básica para leitura de arquivo txt usando java.io

---
+++ src/br/com/ilp010/aula24/LeTexto.java
@@ -0,0 +1,29 @@
+package br.com.ilp010.aula24;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class LeTexto {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println("Informe o nome do arquivo: ");
+			String arquivo;
+			arquivo = scan.next();
+			// Biblioteca para uso da leitura e escrita
+			// BufferedReader é o duto, e a aplicação controla o arquivo pelo duto de
+			// passagem.
+			BufferedReader br = new BufferedReader(new FileReader(arquivo + ".txt"));
+			String linha;
+			int l = 1;
+			while ((linha = br.readLine()) != null) {
+				System.out.println(l + ": " + linha);
+				l++;
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
