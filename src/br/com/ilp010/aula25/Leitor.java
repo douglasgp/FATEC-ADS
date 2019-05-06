b9e39428ba883b8f2b57d1d952cf35c2d84b3e0a
dougl_000 <dougl_000@192.168.5.140>
2019-05-06 01:19:23 -0700

    Aplicação Java para leitura de arquivo de dados com array.

---
+++ src/br/com/ilp010/aula25/Leitor.java
@@ -0,0 +1,31 @@
+package br.com.ilp010.aula25;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class Leitor {
+	public static void main(String[] args) throws Exception {
+		// Abrir catálogo
+		DataInputStream dis = new DataInputStream(new FileInputStream("produto.db"));
+		// Leitura do tamanho
+		int TAMANHO = dis.readInt();
+		System.out.println("Tamanho " + TAMANHO);
+		// dEFINIR arrays
+		int[] codigo = new int[TAMANHO];
+		String[] nome = new String[TAMANHO];
+		double[] preco = new double[TAMANHO];
+		String[] descricao = new String[TAMANHO];
+		// Leitura ddos itens
+		for (int i = 0; i < TAMANHO; i++) {
+			codigo[i] = dis.readInt();
+			nome[i] = dis.readUTF();
+			preco[i] = dis.readDouble();
+			descricao[i] = dis.readUTF();
+			System.out.printf("%06d | %-15s | %7.2f | %s\n", codigo[i], nome[i], preco[i], descricao[i]);
+		}
+		// Fecha arquivo
+		dis.close();
+		System.out.println("Arquivo fechado!");
+	}
+
+}
