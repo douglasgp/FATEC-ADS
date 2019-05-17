d37cb6d5f5a9c8f0c2145c27c8a410349aac7f10
dougl_000 <dougl_000@POS-NOT-WIN8>
2019-05-16 21:04:51 -0700

    Solicita do usuário nome do arquivo a ser lido em Java

--- src/br/com/ilp010/aula26/LeArquivo.java
+++ src/br/com/ilp010/aula26/LeArquivo.java
@@ -2,10 +2,15 @@ package br.com.ilp010.aula26;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class LeArquivo {
 	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new FileReader("etiqueta.txt"));
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o nome do arquivo para leitura: ");
+		String arquivo = scan.next();
+		
+		BufferedReader bf = new BufferedReader(new FileReader(arquivo + ".db"));
 		String linha = bf.readLine();
 		int l = 0;
 		while ((linha = bf.readLine()) != null) {
