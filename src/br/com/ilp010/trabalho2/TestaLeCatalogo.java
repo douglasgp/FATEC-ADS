af4151214b83fc5400b007fbec05aaaa29a7f6c7
Douglas Galdino <douglasgaldino22@hotmail.com>
2019-05-16 23:15:24 -0300

    Leitura completa de arquivo gerado com Java IO

---
+++ src/br/com/ilp010/trabalho2/TestaLeCatalogo.java
@@ -0,0 +1,31 @@
+package br.com.ilp010.trabalho2;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class TestaLeCatalogo {
+	public static void main(String[] args) throws Exception {
+		// Abrir catálogo
+		DataInputStream dis = new DataInputStream(new FileInputStream("src/br/com/ilp010/trabalho2/catalogo01.db"));
+		// Leitura do tamanho
+		int TAMANHO =  dis.readInt();
+		System.out.println("Tamanho " + TAMANHO);
+		// Definir arrays
+		int[] codigo = new int[TAMANHO];
+		String[] nome = new String[TAMANHO];
+		double[] preco = new double[TAMANHO];
+		String[] descricao = new String[TAMANHO];
+		// Leitura dos itens
+		System.out.printf("%4.3s | %-20.8s | %7.9s | %s\n", "CÓD", "NOME", "PREÇO", "DESCRIÇÃO");
+		for(int i = 0; i < TAMANHO; i++) {
+			codigo[i] = dis.readInt();
+			nome[i] = dis.readUTF();
+			preco[i] = dis.readDouble();
+			descricao[i] = dis.readUTF();
+			System.out.printf("%04d | %-20s | %7.2f | %s\n", codigo[i], nome[i], preco[i], descricao[i]);
+		}
+		// Fecha arquivo
+		dis.close();
+		System.out.println("Arquivo fechado!");
+	}
+}
