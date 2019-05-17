8e6bde56673b7cdc92b55ea7e81c37c510b6d77d
dougl_000 <dougl_000@POS-NOT-WIN8>
2019-05-16 21:03:22 -0700

    Leitura de arquivo usando estrutura IO em Java

---
+++ src/br/com/ilp010/aula22/LeituraTexto.java
@@ -0,0 +1,36 @@
+package br.com.ilp010.aula22;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class LeituraTexto {
+	public static void main(String a[]) {
+		if(a.length == 0) {
+			System.out.println("uso:\nJava LeituraTexto <nomeArquivo>");
+			System.exit(-1);
+		}
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(a[0]));
+			System.out.println("Arquivo '" + a[0] + "' aberto.");
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+			System.exit(-1);
+		}
+		try {
+			String line;
+			System.out.println("=== Conteudo ===");
+			// Leitura do stream
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			System.out.println("=== Conteúdo ===");
+			br.close();
+			System.out.println("Arquivo '" + a[0] + "' fechado.");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
