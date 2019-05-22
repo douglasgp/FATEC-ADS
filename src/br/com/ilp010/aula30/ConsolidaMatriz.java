82038a63e26c2b62ea8a6fd7a2e15d8b3a31ff96
NoteP000 <NoteP000@Notep167.ftcbp.edu.br>
2019-05-22 12:59:55 -0300

    Intrudução de cálculo de matriz com Java (INCOMPLETO)

---
+++ src/br/com/ilp010/aula30/ConsolidaMatriz.java
@@ -0,0 +1,20 @@
+package br.com.ilp010.aula30;
+/*
+ * 1º. Declarar 3 matrizes
+ * 2º. Ler valores para matriz A
+ * 3º. Ler valores para matriz B
+ * 4º. Soma matrizes onde:
+ * 	C[l][c] = A[[l][c] + B[[l][c]
+ */
+
+import java.util.Scanner;
+
+public class ConsolidaMatriz {
+	private static int LINHAS;
+	private static int COLUNAS;
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/br/com/ilp010/aula30/Matriz02.java
@@ -0,0 +1,30 @@
+package br.com.ilp010.aula30;
+
+import java.util.Scanner;
+
+public class Matriz02 {
+	private static int LINHA;
+	private static int COLUNA;
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Linha: ");
+		LINHA = scan.nextInt();
+		
+		System.out.println("Coluna: ");
+		COLUNA = scan.nextInt();
+		// Declara e cria matriz bidimensional
+		int[][] a = new int[LINHA][COLUNA];
+		
+		// Laço para percorrer as linhas da matriz
+		for (int l = 0; l < LINHA; l++) {
+			// laço para percorrer as colunas da matriz
+			for (int c = 0; c < COLUNA; c++) {
+				System.out.print("a["+);
+				a[l][c] = in.nextInt;
+				contador++;
+			}
+		}
+
+	}
+}
