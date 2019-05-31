7b5e750db758151e3de08a5f557566487d73dc64
Douglas Galdino <douglasgaldino22@hotmail.com>
2019-05-31 13:47:19 -0300

    Roteiro para desenvolver funções de matrizes com Java

---
+++ src/br/com/ilp010/aula31/Matriz02.java
@@ -0,0 +1,20 @@
+package br.com.ilp010.aula31;
+
+import java.util.Scanner;
+
+public class Matriz02 {
+	private static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		// 1. Ler tamanho das matrizes NUMLIN e NUMCOL
+		
+		// 2. Criar matrizes A, B e C (com o mesmo tamanho)
+		
+		// 3. Ler matriz A
+		
+		// 4. Ler matriz B
+		
+		// 5. Somar matriz A com matriz B (por posição) colocando o resultado na matriz C.
+		
+		// 6. Exibir o resultado (matriz C).
+	}
+}
