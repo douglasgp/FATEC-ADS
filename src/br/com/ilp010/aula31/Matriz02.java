2247733cb9abb9904fdae5c4cefee5ddfeceeb23
douglasgp <douglasgaldino22@hotmail.com>
2019-06-08 19:42:22 -0300

    Soma de matrizes usando função com Java

--- src/br/com/ilp010/aula31/Matriz02.java
+++ src/br/com/ilp010/aula31/Matriz02.java
@@ -5,16 +5,32 @@ import java.util.Scanner;
 public class Matriz02 {
 	private static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		// 1. Ler tamanho das matrizes NUMLIN e NUMCOL
+		// 1. Solicita do usuário tamanho das matrizes NUMLIN e NUMCOL
+		System.out.println("Informe o nº de linhas: ");
+		int nLinhas = scan.nextInt();
+		System.out.println("Informe o nº de Colunas: ");
+		int nColunas = scan.nextInt();
+		
+		// 1.2 Identificação de matriz
+		String mA = "A";
+		String mB = "B";
+		String res = "Resultado";
 		
 		// 2. Criar matrizes A, B e C (com o mesmo tamanho)
+		double a[][] = MatrizLib.criaMatriz(nLinhas, nColunas);
+		double b[][] = MatrizLib.criaMatriz(nLinhas, nColunas);
+		double resultado[][] = MatrizLib.criaMatriz(nLinhas, nColunas);
 		
 		// 3. Ler matriz A
+		MatrizLib.leMatriz(a, mA);
 		
 		// 4. Ler matriz B
+		MatrizLib.leMatriz(b, mB);
 		
-		// 5. Somar matriz A com matriz B (por posi��o) colocando o resultado na matriz C.
+		// 5. Somar matriz A com matriz B (por posição) colocando o resultado na matriz C.
+		MatrizLib.somaMatriz(a, b, resultado);
 		
 		// 6. Exibir o resultado (matriz C).
+		MatrizLib.exibeMatriz(resultado, res);
 	}
 }
--- src/br/com/ilp010/aula31/MatrizLib.java
+++ src/br/com/ilp010/aula31/MatrizLib.java
@@ -1,6 +1,44 @@
 package br.com.ilp010.aula31;
 
+import java.util.Scanner;
+
 public class MatrizLib {
-	
+	public static Scanner scan = new Scanner(System.in);
+
+	// Função para criar matriz
+	public static double[][] criaMatriz(int nLin, int nCol) {
+		return new double[nLin][nCol];
+	}
+
+	// Função para ler matriz
+	public static void leMatriz(double[][] mat,String id) {
+		System.out.println("= = = = = Matriz - "+id+" = = = = = ");
+		for (int lin = 0; lin < mat.length; lin++) {
+			for (int col = 0; col < mat[lin].length; col++) {
+				System.out.println("Mat"+id+"[" + lin + "][" + col + "]:");
+				mat[lin][col] = scan.nextDouble();
+			}
+		}
+	}
+
+	// Função para somar matriz
+	public static void somaMatriz(double[][] matA, double[][] matB, double[][] AmaisB) {
+		for (int lin = 0; lin < AmaisB.length; lin++) {
+			for (int col = 0; col < AmaisB[lin].length; col++){
+				AmaisB[lin][col] = matA[lin][col] + matB[lin][col];
+			}			
+		}
+	}
+
+	// Função para exibie matriz
+	public static void exibeMatriz(double[][] AmaisB, String id) {
+		System.out.println("= = = = = "+id+" = = = = = ");
+		for (int lin = 0; lin < AmaisB.length; lin++) {
+			for (int col = 0; col < AmaisB[lin].length; col++) {
+				System.out.printf("%6.1f | ", AmaisB[lin][col]);
+			}
+			System.out.println();
+		}
+	}
 
 }
