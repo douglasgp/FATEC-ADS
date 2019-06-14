9d78b69522e5260ea1da7b0a1df4fbe37f1cc12f
douglasgp <douglasgaldino22@hotmail.com>
2019-06-14 12:31:42 -0300

    Aumento na quantidade de itens de catálogo

--- src/br/com/ilp010/trabalho2/TesteGeraCatalogo.java
+++ src/br/com/ilp010/trabalho2/TesteGeraCatalogo.java
@@ -8,16 +8,20 @@ public class TesteGeraCatalogo {
 
 	public static void main(String[] args) throws Exception {
 
-		int[] codigo = { 7654, 6543, 5432, 4321, 4322, 7643, 6143, 5492, 4331, 4382 };
-		String[] nome = { "Ra��o Para gatos", "Ra��o para cachorro", "Areia Higi�nica", "Coleira",
-				"Shampoo c�es e gato", "Ra��o Para Gatos", "Ra��o para cachorro", "Cama Pet", "Colonia C�es e gatos",
-				"Tapete Higi�nico" };
-		double[] preco = { 12.90, 12.90, 5.90, 10.90, 12.90, 12.90, 12.90, 25.00, 12.00, 18.00 };
-		String[] descricao = { "Golden, Adulto, 1kg", "Golden, Adulto, 1kg", "CareCat, Higi�nica Mineral, 4kg",
+		int[] codigo = { 7654, 6543, 5432, 4321, 4322, 7643, 6143, 5492, 4331, 4382, 8654, 9543, 4432, 3321, 2322, 2643, 1143, 1492, 1331, 4392  };
+		String[] nome = { "Raçãoo Para gatos", "Raçãoo para cachorro", "Areia Higiênica", "Coleira",
+				"Shampoo cães e gato", "Ração Para Gatos", "Raçãoo para cachorro", "Cama Pet", "Colonia Cães e gatos",
+				"Tapete Higiênico", "Ração Para gatos", "Raçãoo para cachorro", "Areia Higiênica", "Coleira",
+				"Shampoo cães e gato", "Ração Para Gatos", "Raçãoo para cachorro", "Cama Pet", "Colonia Cães e gatos",
+				"Tapete Higiênico" };
+		double[] preco = { 12.90, 12.90, 5.90, 10.90, 12.90, 12.90, 12.90, 25.00, 12.00, 18.00, 32.50, 22.95, 51.93, 30.67, 19.34, 12.19, 23.95, 25.83, 12.64, 65.91 };
+		String[] descricao = { "Golden, Adulto, 1kg", "Golden, Adulto, 1kg", "CareCat, Higiênica Mineral, 4kg",
 				"Pet, azul, Adulto", "Sanol, neutro, 200ml", "Golden, Filhote, 1kg", "Golden, Filhote, 1kg",
-				"PetBamboo, Anti Al�rgico, Pequeno", "PetBamboo, Anti Pulgas, 200ml", "Fofo Pet, Adulto, 7 unidades" };
+				"PetBamboo, Anti Alérgico, Pequeno", "PetBamboo, Anti Pulgas, 200ml", "Fofo Pet, Adulto, 7 unidades", "Golden, Filhote, 1.5kg", "Golden, Filhote, 1.5kg", "CareCat, Higiênica Mineral, 4kg",
+				"Pet, Branco, Filhote", "Sanol, neutro, 500ml", "Golden, Adulto, 1.5kg", "Golden, Adulto, 15kg",
+				"PetBamboo, Anti Alérgico, Grande", "PetBamboo, Anti Pulgas, 500ml", "Fofo Pet, Adulto, 20 unidades" };
 
-		DataOutputStream dos = new DataOutputStream(new FileOutputStream("src/br/com/ilp010/trabalho2/catalogo01.db"));
+		DataOutputStream dos = new DataOutputStream(new FileOutputStream("/home/douglasgp/Público/produto.db"));
 		System.out.println("Arquivo aberto");
 
 		dos.writeInt(TAMANHO);
