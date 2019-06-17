03a721ca22cf43f59a856aab2a509263cd0889f8
douglasgp <douglasgaldino22@hotmail.com>
2019-06-17 14:13:18 -0300

    Link de referência em comentário no código (Java)

--- src/br/com/review/Principal.java
+++ src/br/com/review/Principal.java
@@ -1,5 +1,6 @@
 package br.com.review;
 /*
+ * https://www.geeksforgeeks.org/calendar-class-in-java-with-examples/
  * https://www.devmedia.com.br/forum/somar-10-dias-a-uma-data/565299
  */
 import java.util.Scanner;
