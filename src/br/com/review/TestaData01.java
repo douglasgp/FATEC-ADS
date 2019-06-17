7fcca62554a0a0c10b6b9269b2c10c0c13c9f273
douglasgp <douglasgaldino22@hotmail.com>
2019-06-17 14:07:46 -0300

    Teste de biblioteca Java para datas (GregorianCalendar)

--- src/br/com/review/TestaData01.java
+++ src/br/com/review/TestaData01.java
@@ -2,7 +2,9 @@ package br.com.review;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class TestaData01 {
@@ -11,32 +13,20 @@ public class TestaData01 {
 	public static void main(String[] args) throws Exception {
 		int numAulas = 2;
 		int numRevisao = 6;
-		Date[] dataTeste1 = new Date[numAulas];
+		Calendar[] dataTeste1 = new GregorianCalendar[numAulas];
 		Date[][] matrizData = new Date[numAulas][numRevisao];
 
-		String vetorDatasStr[] = { "02/01/2019", "15/05/2019" };
+		GregorianCalendar dataTeste2[] = {new GregorianCalendar(2019 ,01 ,02)};
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 
 		for (int l = 0; l < numAulas; l++) {
-			dataTeste1[l] = df.parse(vetorDatasStr[l]);
+			
 		}
 
 		for (int l = 0; l < numAulas; l++) {
 			System.out.println(df.format(dataTeste1[l]));
 			for (int c = 1; c <= numRevisao; c++) {
-				if (c == 1) {
-					matrizData[l][c] = dataTeste1[l];
-				} else if (c == 2) {
-					matrizData[l][c] = dataTeste1[l + 1];
-				} else if (c == 3) {
-					matrizData[l][c] = dataTeste1[l + 6];
-				} else if (c == 4) {
-					matrizData[l][c] = dataTeste1[l + 23];
-				} else if (c == 5) {
-					matrizData[l][c] = dataTeste1[l + 30];
-				} else if (c == 6) {
-					matrizData[l][c] = dataTeste1[l + 30];
-				}
+				
 			}
 		}
 
